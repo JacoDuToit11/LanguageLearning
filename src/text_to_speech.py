afe585cdea7f8765adf2e24c4d7083f19cd3ac75
Jaco Wynand Du Toit <22808892@sun.ac.za>
2024-08-24 10:39:16 +0200

    basic text to speech done

---
+++ src/text_to_speech.py
@@ -0,0 +1,35 @@
+from openai import OpenAI
+import os
+
+difficulty_level = 'beginner'
+# difficulty_level = 'intermediate'
+language = 'German'
+
+text_lessons_path = f'../data/{language}/text/{difficulty_level}/'
+speech_lessons_path = f'../data/{language}/speech/{difficulty_level}/'
+
+openai_client = OpenAI(api_key=os.environ['JACO_OPENAI_API_KEY'])
+speech_model = 'tts-1'
+
+def main():
+    text_to_speech()
+
+def text_to_speech():
+    lessons_text_files = sorted(os.listdir(text_lessons_path))
+    lessons_text_files.remove('combined_lessons.txt')
+
+    for text_file in lessons_text_files:
+        with open(f'{text_lessons_path}{text_file}', 'r') as file:
+            text = file.read()
+        
+        print(text)
+        response = openai_client.audio.speech.create(
+            model=speech_model,
+            voice='onyx',
+            input=text
+        )
+        response.stream_to_file(f'{speech_lessons_path}{text_file[:-3]}mp3')
+        break
+
+if __name__ == '__main__':
+    main()
