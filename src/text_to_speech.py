2c87156817d41a140369fb24c78c34493dbac2df
Jaco Wynand Du Toit <22808892@sun.ac.za>
2024-08-31 16:08:20 +0200

    added pauses

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,5 +1,8 @@
 from openai import OpenAI
-import os
+import os, json
+from pydub import AudioSegment
+silence_duration1 = 2000 # milliseconds
+silence_duration2 = 3000 # milliseconds
 
 difficulty_level = 'beginner'
 # difficulty_level = 'intermediate'
@@ -14,27 +17,59 @@ openai_client = OpenAI(api_key=os.environ['JACO_OPENAI_API_KEY'])
 speech_model = 'tts-1'
 
 def main():
-    text_to_speech()
-
-def text_to_speech():
-    # TODO redo this
-    lessons_text_files = sorted(os.listdir(text_lessons_path))
-    lessons_text_files.remove('combined_lessons.txt')
-
-    for text_file in lessons_text_files:
-        with open(f'{text_lessons_path}{text_file}', 'r') as file:
-            text = file.read()
-        
-        text = text[:1000]
-        print(text)
+    with open(f'{text_lessons_path}combined_lessons.json', 'r') as file:
+        lessons_dict = json.load(file)
+    
+    for lesson_num, lesson_content in lessons_dict.items():
+        text_to_speech(lesson_num, lesson_content)
         exit()
-        response = openai_client.audio.speech.create(
+
+def text_to_speech(lesson_num, lesson_content):
+    final_audio = AudioSegment.silent(duration=0)
+    silence_segment1 = AudioSegment.silent(duration=silence_duration1)
+    silence_segment2 = AudioSegment.silent(duration=silence_duration2)
+
+    for i in range(0, len(lesson_content), 2):
+
+        response1 = openai_client.audio.speech.create(
             model=speech_model,
-            voice='onyx',
-            input=text
+            voice="onyx", 
+            input=lesson_content[i],
         )
-        response.stream_to_file(f'{speech_lessons_path}{text_file[:-3]}mp3')
-        break
+
+        # Save and load each segment
+        temp_audio_file1 = f"tmp1.mp3"
+        with open(temp_audio_file1, "wb") as f:
+            f.write(response1.content) 
+
+        response2 = openai_client.audio.speech.create(
+            model=speech_model,
+            voice="onyx", 
+            input=lesson_content[i+1],
+        )
+
+        temp_audio_file2 = f"tmp2.mp3"
+        with open(temp_audio_file2, "wb") as f:
+            f.write(response2.content) 
+
+        segment1_audio = AudioSegment.from_file(temp_audio_file1)
+        segment2_audio = AudioSegment.from_file(temp_audio_file2)
+
+        final_audio += segment1_audio
+        final_audio += silence_segment1
+        final_audio += segment2_audio
+
+        if i > 0:
+            final_audio += silence_segment1
+            final_audio += segment1_audio
+            final_audio += silence_segment2
+            final_audio += segment2_audio
+        final_audio += silence_segment1
+
+        # Clean up the temporary file
+        os.remove(temp_audio_file1)
+        os.remove(temp_audio_file2)
+    final_audio.export(f"{speech_lessons_path}lesson_{lesson_num}.mp3", format="mp3")
 
 if __name__ == '__main__':
     main()
